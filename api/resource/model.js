8c14ed963b60ea8cb208a5950b839fa49c220e36
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-02-18 15:54:24 -0500

    finished resource folder

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,19 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const getAll = () => {
+    return db('resources')
+}
+
+const create = (resource) => {
+    return db('resources')
+    .insert(resource)
+    .then(([resource_id]) => {
+        return getAll().where({ resource_id }).first()
+    })
+}
+
+module.exports = {
+    getAll,
+    create
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,22 @@
 // build your `/api/resources` router here
 const router = require('express').Router()
+const Resources = require('./model')
+
+router.get('/', (req, res, next) => {
+    Resources.getAll()
+    .then(resource => {
+        res.status(200).json(resource)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Resources.create(req.body)
+    .then(newProject => {
+        res.status(201).json(newProject)
+    })
+    .catch(next)
+})
 
 router.use((err, req, res, next) => {//eslint-disable-line
     res.status(500).json({
